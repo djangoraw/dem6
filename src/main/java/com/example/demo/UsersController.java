9869e015ea7e1774016125494bb94a4ba213b41a
joanna.sliwa <pretty.v4cant@gmail.com>
2022-04-09 16:01:53 +0200

    commit uno

--- src/main/java/com/example/demo/UsersController.java
+++ src/main/java/com/example/demo/UsersController.java
@@ -1,13 +1,20 @@
-/*
+
 package com.example.demo;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class UsersController {
 
-    private final Map<Integer,UserEntity> users = Map.of(1,new UserEntity("Tom"), 2, new UserEntity("John"));
+    private final Map<Integer,UserEntity> users = Map.of(1,new UserEntity("Tim"), 2, new UserEntity("Jim"));
 
     @GetMapping("/users/all")
     public ResponseEntity<Collection<UserEntity>> getApiUser2() {
@@ -18,9 +25,8 @@ public class UsersController {
     public ResponseEntity<UserEntity> getApiUser2(
             @PathVariable("id") int id
 
-            ) {
+    ) {
         return ResponseEntity.of(Optional.ofNullable(users.get(id)));
-}
+    }
 
 }
-*/
